a3b99a56883f958a06f7380a52f27080eb973569
chhaichivon <chhaichivon1995@gmail.com>
2017-10-25 06:13:10 +0700

    ui home

--- app/src/main/java/com/chhaichivon/movieandroidapp/MainActivity.java
+++ app/src/main/java/com/chhaichivon/movieandroidapp/MainActivity.java
@@ -1,12 +1,15 @@
 package com.chhaichivon.movieandroidapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
-	String str = "Hello Mr Chivon";
+	Button  mLoginButton;
 
 
 	@Override
@@ -14,8 +17,14 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		TextView  textView  = (TextView) findViewById(R.id.txtMessage);
-		textView.setText(str);
+		mLoginButton = (Button) findViewById(R.id.btn_login);
+		mLoginButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent  intent  = new Intent(getApplicationContext(), BottomNavigationActivity.class);
+				startActivity(intent);
+			}
+		});
 		
 	}
 }
